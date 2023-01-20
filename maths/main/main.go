b58c677261ba2857b120b92e1bc07118e6665b8d
ayvillalobos <avillalobos@wawand.co>
2023-01-20 16:32:20 -0500

    cambio de nombre

---
+++ maths/main/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func addtion(a, b int) int {
+	return a + b
+}
+
+func subtraction(a, b int) int {
+	return a - b
+}
+
+func multiplication(a, b int) int {
+	return a * b
+}
+
+func division(a, b int) int {
+	return a / b
+}
+
+func validate(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+
+}
+
+func calculate(a, b int) {
+	addtion := addtion(a, b)
+	subtraction := subtraction(a, b)
+	multiplication := multiplication(a, b)
+	division := division(a, b)
+	validate := validate(a, b)
+
+	fmt.Printf("Addition: %v, Subtraction: %v, Multiplication: %v, Division  %v, The Number Major Is: %v\n ", addtion, subtraction, multiplication, division, validate)
+
+}
+
+func main() {
+	calculate(5, 5)
+	calculate(5, 4)
+
+}
