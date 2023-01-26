3884f79dc6d2242becca71ecfd9f88926b610270
ayvillalobos <avillalobos@wawand.co>
2023-01-26 17:59:54 -0500

    ejercicio terminado

--- matriz/main/main.go
+++ matriz/main/main.go
@@ -5,41 +5,45 @@ import (
 	"sort"
 )
 
-func main() {
-
-	var rows, columns int
-	fmt.Println("enter a number")
-	fmt.Scan(&rows)
-	fmt.Println("enter a number")
-	fmt.Scan(&columns)
-	var mat = make([][]int, columns)
-
+func matriz(columns, rows int) {
+	mat := make([][]int, columns)
+	var lista []int
 	for f := 0; f < columns; f++ {
 		mat[f] = make([]int, rows)
 		for c := 0; c < rows; c++ {
-			fmt.Print("Enter the numbers: ")
-			fmt.Scan(&mat[f][c])
+			var num int
+			fmt.Printf("Enter the numbers in posicion: ")
+			fmt.Scan(&num)
+
+			lista = append(lista, num)
+
 		}
-		fmt.Println(mat)
+		sort.Ints(lista)
+
+	}
+
+	for i := 0; i < columns; i++ {
+		for j := 0; j < rows; j++ {
+			mat[i][j] = lista[(i*rows)+j]
+
+		}
 	}
 
 	for _, v := range mat {
-		sort.Ints(v)
+
 		fmt.Println(v)
 
 	}
-
 }
 
-func ordenarMenor(listNum []int, Cant int) {
-	tmp := 0
-	for x := 0; x < Cant; x++ {
-		for y := 0; y < Cant; y++ {
-			if listNum[x] < listNum[y] {
-				tmp = listNum[y]
-				listNum[y] = listNum[x]
-				listNum[x] = tmp
-			}
-		}
-	}
+func main() {
+
+	var rows, columns int
+	fmt.Println("enter a number the rows")
+	fmt.Scan(&rows)
+	fmt.Println("enter a number the columns")
+	fmt.Scan(&columns)
+
+	matriz(columns, rows)
+
 }
