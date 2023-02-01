18b7fa102ddd27067087d22c9fe15273e7dc3b9b
ayvillalobos <avillalobos@wawand.co>
2023-02-01 10:26:38 -0500

    cambios

--- word/main/main.go
+++ word/main/main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	var phrasal string
-	var word string
-	fmt.Println("Enter a text:")
+	//var phrasal string
+	//var word string
+	/*
+		fmt.Print("Enter a text:")
 
-	fmt.Scan(&phrasal)
-	fmt.Println("Enter a word:")
-	fmt.Scan(&word)
+		fmt.Scan(&phrasal)
+		fmt.Println("Enter a word")
+		fmt.Scan(&word)*/
+	fmt.Print("Enter a text:")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	text := scanner.Text()
+	fmt.Println(text)
+	fmt.Println("Enter a word")
+	//fmt.Scan(&word)
+	scanner2 := bufio.NewScanner(os.Stdin)
+	scanner2.Scan()
+	texto := scanner.Text()
+	fmt.Println(texto)
 
-	err := countWord(phrasal, word)
+	err := countWord(text, texto)
 	fmt.Println(err)
+
 	/*var refString = "maria es linda maria maria"
 	var word = "maria"
 	palabras := strings.Split(refString, " ")
@@ -27,16 +42,15 @@ func main() {
 
 func countWord(phrasal, word string) string {
 
-	a := strings.Split(phrasal, ",")
+	a := strings.Split(phrasal, " ")
 
-	fmt.Println(a)
-	fmt.Sprintln(a)
 	for _, v := range a {
 		if v == word {
 			count := strings.Count(phrasal, word)
 			return fmt.Sprintln("The word", word, "appears ", count, "times in the text.")
 		}
-
+		fmt.Println(v)
+		fmt.Println("posicion", len(v))
 	}
 
 	return fmt.Sprintln("That word is not in the text.")
