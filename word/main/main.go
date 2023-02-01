f92ca86dc96483eeed3b4ab14a68cbd8642342fd
ayvillalobos <avillalobos@wawand.co>
2023-02-01 11:55:08 -0500

    ejercicio finalizado

--- word/main/main.go
+++ word/main/main.go
@@ -8,35 +8,18 @@ import (
 )
 
 func main() {
-	//var phrasal string
-	//var word string
-	/*
-		fmt.Print("Enter a text:")
 
-		fmt.Scan(&phrasal)
-		fmt.Println("Enter a word")
-		fmt.Scan(&word)*/
-	fmt.Print("Enter a text:")
+	fmt.Println("Enter a text: ")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	text := scanner.Text()
-	fmt.Println(text)
-	fmt.Println("Enter a word")
-	//fmt.Scan(&word)
+	phrasal := scanner.Text()
+	fmt.Println("Enter a word ")
 	scanner2 := bufio.NewScanner(os.Stdin)
 	scanner2.Scan()
-	texto := scanner.Text()
-	fmt.Println(texto)
+	word := scanner2.Text()
 
-	err := countWord(text, texto)
+	err := countWord(phrasal, word)
 	fmt.Println(err)
-
-	/*var refString = "maria es linda maria maria"
-	var word = "maria"
-	palabras := strings.Split(refString, " ")
-	for _, palabra := range palabras {
-		fmt.Println(palabra)
-	}*/
 
 }
 
@@ -49,8 +32,7 @@ func countWord(phrasal, word string) string {
 			count := strings.Count(phrasal, word)
 			return fmt.Sprintln("The word", word, "appears ", count, "times in the text.")
 		}
-		fmt.Println(v)
-		fmt.Println("posicion", len(v))
+
 	}
 
 	return fmt.Sprintln("That word is not in the text.")
